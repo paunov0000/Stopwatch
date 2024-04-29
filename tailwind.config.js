b0fd1596e2c54e2078cf69253f647b2e7fe5797d
paunov0000 <legendarygriefer96@gmail.com>
2024-04-29 21:59:26 +0300

    Added 3 custom colors, their only purpose is time display gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gradient1: "#274B74",
+        gradient2: "#8233C5",
+        gradient3: "#E963FD",
+      },
+    },
   },
   plugins: [],
-}
-
+};
